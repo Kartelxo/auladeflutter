04d366262fe83dc17fcf593d3740327064162726
Rodrigo <rodrigoetomas22@gmail.com>
2026-04-17 11:31:48 +0100

    adição das classes

--- aula_flutter/lib/main.dart
+++ aula_flutter/lib/main.dart
@@ -4,3 +4,15 @@ import 'app.dart';
 void main() {
   runApp(const MyApp());
 }
+
+class My app extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: PeoplePage(),
+    );
+  }
+}
